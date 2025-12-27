74022dfaaea2f1d98a9d795218c51bea841343c9
kimminchae1 <minchaeg10@gmail.com>
2025-12-28 02:32:49 +0900

    frontend 구조 변경 및 수정

--- frontend/src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-  return <button>헤더</button>;
-};
-
-export default Header;
--- frontend/src/pages/machine/Machine.jsx
+++ frontend/src/pages/machine/Machine.jsx
@@ -1,5 +1,12 @@
+import MachineList from "./components/MachineList";
+import PageLayout from "../../components/layout/PageLayout";
+
 const Machine = () => {
-  return <div>Machine</div>;
+  return (
+    <PageLayout>
+      <MachineList />
+    </PageLayout>
+  );
 };
 
 export default Machine;
