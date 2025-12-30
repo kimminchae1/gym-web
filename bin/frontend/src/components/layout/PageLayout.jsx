7d1d503bc5cce0e0b9dd2ddefd5209d22805abfa
younho-7878 <younho2300@gmail.com>
2025-12-30 18:15:31 +0900

    chore: remove tracked bin directory

--- bin/frontend/src/components/layout/PageLayout.jsx
+++
@@ -1,7 +0,0 @@
-import "./PageLayout.css";
-
-const PageLayout = ({ children }) => {
-  return <main className="page_layout">{children}</main>;
-};
-
-export default PageLayout;
--- bin/frontend/src/pages/post/PostWrite.jsx
+++
@@ -1,5 +0,0 @@
-const PostWrite = () => {
-  return <div>PostWrite</div>;
-};
-
-export default PostWrite;
